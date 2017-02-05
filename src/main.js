2449038fac1ab4c5638006a089010e7bb78b5572
linux <arjunkoid@gmail.com>
2017-02-05 17:19:06 +0000

    Added react, react-dom etc and home page

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 $ = jQuery = require('jquery');
+
+var React = require('react');
+var ReactDom = require('react-dom');
+var Home = require('./components/homePage');
+
 var App = console.log("Hello world from browserfy"); 
 
-module.exports = App; 
+
+
+ReactDom.render(<Home />, document.getElementById('root'));
