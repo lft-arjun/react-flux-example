18bd80fa54b6cedc059b065ea2e7079a364c94cf
linux <arjunkoid@gmail.com>
2017-02-17 10:15:03 +0000

    Added active class

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -12,10 +12,10 @@ var Header = React.createClass({
 							<img src="images/small-logo.png" alt="pint" />
 						</Link>
 						<ul className="nav navbar-nav">
-							<li> <Link to="/">Home</Link></li>
-							<li> <Link to="/about">About</Link></li>
-							<li> <Link to="/authors">Authors</Link></li>
-							<li> <Link to="/users">Users</Link></li>
+							<li> <Link to="/" className="active">Home</Link></li>
+							<li> <Link to="/about" className="active">About</Link></li>
+							<li> <Link to="/authors" className="active">Authors</Link></li>
+							<li> <Link to="/users" className="active" >Users</Link></li>
 
 						</ul>
 					</div>
